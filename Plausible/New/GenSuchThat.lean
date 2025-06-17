c158eeff6d8a4b3eca3240a000a20c2d58eaaabb
ngernest <eyn5@cornell.edu>
2025-06-17 14:44:22 -0400

    Typeclass instance for going from GenSizedSuchThat -> GenSuchThat

--- Plausible/New/GenSuchThat.lean
+++ Plausible/New/GenSuchThat.lean
@@ -1,5 +1,16 @@
+import Plausible.Gen
 import Plausible.New.OptionTGen
+open Plausible
 
-/-- Generators of type [α] such that [P : α -> Prop] holds for all generated values -/
+/-- Sized generators of type [α] such that [P : α -> Prop] holds for all generated values -/
 class GenSizedSuchThat (α : Type u) (P : α → Prop) where
   genSizedST : Nat → OptionT Gen α
+
+/-- Generators of type [α] such that [P : α -> Prop] holds for all generated values -/
+class GenSuchThat (α : Type u) (P : α → Prop) where
+  genST : OptionT Gen α
+
+/-- Every `GenSizedSuchThat` instance can be automatically given a `GenSuchThat` instance
+    using the `OptionTGen.sized` combinator -/
+instance [GenSizedSuchThat α P] : GenSuchThat α P where
+  genST := OptionTGen.sized (GenSizedSuchThat.genSizedST P)
