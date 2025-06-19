92968236148a2d98b31d201930c80ffcaa643761
ngernest <eyn5@cornell.edu>
2025-06-19 17:33:13 -0400

    Comment out the time-consuming tests from the original Plausible codebase for now

--- Test.lean
+++ Test.lean
@@ -3,8 +3,8 @@ Copyright (c) 2024 Lean FRO, LLC. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Henrik Böving
 -/
-import Test.Tactic
-import Test.Testable
+-- import Test.Tactic
+-- import Test.Testable
 import Test.DeriveBSTGenerator
 import Test.DeriveBalancedTreeGenerator
 import Test.DeriveRegexGenerator
