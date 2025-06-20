6428ff4ee5e2e5d7868c6f74e635bef87b877dde
Ernest Ng <eyn5@cornell.edu>
2025-06-20 18:51:22 -0400

    Rename GenSized to ArbitrarySized, GenUnsized -> Arbitrary

--- Plausible/New/GenSized.lean
+++ Plausible/New/GenSized.lean
@@ -4,19 +4,22 @@ import Plausible.Sampleable
 
 open Plausible
 
-/-- This is the equivalent of QuickChick's `Gen` typeclass / Haskell QuickCheck's `Arbitrary` typeclass -/
-class GenUnsized (α : Type) where
+/-- This is the equivalent of Haskell QuickCheck's `Arbitrary` typeclass.
+    (In QuickChick, this typeclass is called `Gen`, but `Gen` is already
+    a reserved keyword in Plausible, so we call this typeclass `Arbitrary`
+    following the Haskell QC convention) -/
+class Arbitrary (α : Type) where
   gen : Gen α
 
 /-- Equivalent to QuickChick's `GenSized` typeclass -/
-class GenSized (α : Type) where
+class ArbitrarySized (α : Type) where
   genSized : Nat → Gen α
 
 /-- Every `GenSized` instance gives rise to a `SampleableExt` instance
     - Note: Plausible's `SampleableExt` is analogous to QuickChick's `Arbitrary` typeclass
       (which combines QuickChick's `Gen` and `Shrink` typeclass)-/
-instance [Repr α] [Shrinkable α] [GenSized α] : SampleableExt α :=
-  SampleableExt.mkSelfContained (GeneratorCombinators.sized GenSized.genSized)
+instance [Repr α] [Shrinkable α] [ArbitrarySized α] : SampleableExt α :=
+  SampleableExt.mkSelfContained (GeneratorCombinators.sized ArbitrarySized.genSized)
 
 -- TODO: figure out how to distinguish between `GenSized` and `GenSizedSuchThat` in Chamelean
 
