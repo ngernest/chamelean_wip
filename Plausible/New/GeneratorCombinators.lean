ee58ea35223402d2c83f3c31abf303a5e628f62f
ngernest <eyn5@cornell.edu>
2025-06-24 16:42:37 -0400

    Add oneOf generator combinator that doesn't require proofs of in-bounds indexing

--- Plausible/New/GeneratorCombinators.lean
+++ Plausible/New/GeneratorCombinators.lean
@@ -22,6 +22,18 @@ def pick (default : Gen α) (xs : List (Nat × Gen α)) (n : Nat) : Nat × Gen �
 def sumFst (gs : List (Nat × α)) : Nat :=
   List.foldl (fun acc p => acc + p.fst) 0 gs
 
+/-- Picks one of the generators in `gs` at random, returning the  `default` generator
+    if `gs` is empty.
+
+    (This is a more ergonomic version of Plausible's `Gen.oneOf` which doesn't
+    require the caller to supply a proof that the list index is in bounds.) -/
+def oneOfWithDefault (default : Gen α) (gs : List (Gen α)) : Gen α :=
+  match gs with
+  | [] => default
+  | _ => do
+    let idx ← Gen.choose Nat 0 (gs.length - 1) (by omega)
+    List.getD gs idx.val default
+
 /-- `frequency` picks a generator from the list `gs` according to the weights in `gs`.
     If `gs` is empty, the `default` generator is returned.  -/
 def frequency (default : Gen α) (gs : List (Nat × Gen α)) : Gen α := do
