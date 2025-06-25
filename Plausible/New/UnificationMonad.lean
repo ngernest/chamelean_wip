2fa1c0bc18bb10369a2c7f1e0125f24a57a1842e
ngernest <eyn5@cornell.edu>
2025-06-25 17:13:43 -0400

    Comments

--- Plausible/New/UnificationMonad.lean
+++ Plausible/New/UnificationMonad.lean
@@ -34,16 +34,19 @@ structure UnifyState where
 /-- Under the hood, this means `Unify α := UnifyState → Option (α × UnifyState)` -/
 abbrev Unify (α : Type) := StateT UnifyState Option α
 
+/-- `update u r` sets the range of the unknown `u` to be `r` -/
 def update (u : Unknown) (r : Range) : Unify Unit :=
   fun s =>
     let k := s.constraints
     .some ((), { s with constraints := k.insert u r })
 
+/-- Registers a new equality check between unknowns `u1` and `u2` -/
 def equality (u1 : Unknown) (u2 : Unknown) : Unify Unit :=
   fun s =>
     let eqs := s.equalities
     .some ((), { s with equalities := eqs.merge {(u1, u2)} })
 
+/-- Adds a new pattern match -/
 def pattern (u : Unknown) (p : Pattern) : Unify Unit :=
   fun s =>
     let ps := s.patterns
@@ -54,6 +57,7 @@ def fresh_unknown (unknowns : Std.TreeSet Unknown) : Unknown :=
   let existingNames := Name.mkStr1 <$> unknowns.toArray
   toString $ genFreshName existingNames (Name.mkStr1 "unknown")
 
+/-- Generaets and returns a new unknown -/
 def fresh : Unify Unknown :=
   fun s =>
     let us := s.unknowns
@@ -65,7 +69,7 @@ def fresh : Unify Unknown :=
 -- Unification algorithm (fig. 3)
 mutual
   /-- Unifies two ranges -/
-  def unify : Range → Range → Unify Unit
+  partial def unify : Range → Range → Unify Unit
     | .Unknown u1, .Unknown u2 =>
       if u1 == u2 then
         return ()
@@ -87,17 +91,20 @@ mutual
     | _, _ => failure
 
   /-- Unifies two `(Unknown, Range)` pairs -/
-  def unifyR : Unknown × Range → Unknown × Range → Unify Unit
-    | (u1, r1), (u2, r2) => sorry
+  partial def unifyR : Unknown × Range → Unknown × Range → Unify Unit
+    | (u1, .Undef _), (u2, r2) => update u1 r2
+    | (u1, r1), (u2, .Undef _) => update u2 r1
+    | (u1, u1'@(.Unknown _)), (u2, r2) => unify u1' r2
+    | (u1, r1), (u2, u2') => unify r1 u2'
 
   /-- Unifies two `Range`s that are both constructors -/
-  def unifyC (r1 : Range) (r2 : Range) : Unify Unit :=
+  partial def unifyC (r1 : Range) (r2 : Range) : Unify Unit :=
     match r1, r2 with
     | .Ctr c1 rs1, .Ctr c2 rs2 => sorry
     | _, _ => sorry
 
   /-- Unifies an `(Unknown, Range)` pair with a `Range -/
-  def unifyRC (p1 : Unknown × Range) (r2 : Range) : Unify Unit :=
+  partial def unifyRC (p1 : Unknown × Range) (r2 : Range) : Unify Unit :=
     let (u1, r1) := p1
     match r1, r2 with
     | _, .Ctr c2 rs2 => sorry
