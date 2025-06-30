60bef83c429aba27b1b8b816dc13972be78b816d
ngernest <eyn5@cornell.edu>
2025-06-30 11:16:08 -0400

    Rename LazyList constructors to match QuickChick

--- Plausible/New/LazyList.lean
+++ Plausible/New/LazyList.lean
@@ -4,16 +4,16 @@
 
 /-- Lazy Lists are implemented by thunking the computation for the tail of a cons-cell. -/
 inductive LazyList (α : Type u) where
-  | nil
-  | cons : α → Thunk (LazyList α) → LazyList α
+  | lnil
+  | lcons : α → Thunk (LazyList α) → LazyList α
 deriving Inhabited
 
 namespace LazyList
 
 /-- Converts a Lazy List to an ordinary list by forcing all the embedded thunks -/
 def toList : LazyList α → List α
-  | .nil => []
-  | .cons x xs => x :: xs.get.toList
+  | lnil => []
+  | .lcons x xs => x :: xs.get.toList
 
 /-- We pretty-print `LazyList`s by converting them to ordinary lists
     (forcing all the thunks) & pretty-printing the resultant list. -/
@@ -23,23 +23,19 @@ instance [Repr α] : Repr (LazyList α) where
 /-- Retrieves a prefix of the `LazyList` (only the thunks in the prefix are evaluated) -/
 def take (n : Nat) (l : LazyList α) : LazyList α :=
   match n with
-  | .zero => .nil
+  | .zero => lnil
   | .succ n' =>
     match l with
-    | .nil => .nil
-    | .cons x xs => .cons x (take n' xs.get)
-
-/-- Creates a `LazyList` from a function `f` -/
-def ofFn (f : Fin n → α) : LazyList α :=
-  Fin.foldr n (fun i xs => LazyList.cons (f i) xs) (init := .nil)
+    | lnil => lnil
+    | .lcons x xs => .lcons x (take n' xs.get)
 
 /-- Appends two `LazyLists` together
     (Note: the body of delayed does not need to be an explicit call to `Thunk.mk` because
     Lean automatically coerces any `e : α` into `Thunk.mk (fun () => e) : Thunk α`.) -/
 def append (xs : LazyList α) (ys : LazyList α) : LazyList α :=
   match xs with
-  | .nil => ys
-  | .cons x xs' => .cons x (append xs'.get ys)
+  | lnil => ys
+  | .lcons x xs' => .lcons x (append xs'.get ys)
 
 /-- `observe tag i` uses `dbg_trace` to emit a trace of the variable
     associated with `tag` -/
@@ -50,8 +46,8 @@ def observe (tag : String) (i : Fin n) : Nat :=
 /-- Maps a function over a LazyList -/
 def mapLazyList (f : α → β) (l : LazyList α) : LazyList β :=
   match l with
-  | .nil => .nil
-  | .cons x xs => .cons (f x) (mapLazyList f xs.get)
+  | lnil => lnil
+  | .lcons x xs => .lcons (f x) (mapLazyList f xs.get)
 
 /-- `Functor` instance for `LazyList` -/
 instance : Functor LazyList where
@@ -59,7 +55,7 @@ instance : Functor LazyList where
 
 /-- Creates a singleton LazyList -/
 def pureLazyList (x : α) : LazyList α :=
-  LazyList.cons x $ Thunk.mk (fun _ => LazyList.nil)
+  LazyList.lcons x $ Thunk.mk (fun _ => lnil)
 
 /-- Alias for `pureLazyList` -/
 def singleton (x : α) : LazyList α :=
@@ -68,8 +64,8 @@ def singleton (x : α) : LazyList α :=
 /-- Flattens a `LazyList (LazyList α)` into a `LazyList α`  -/
 def concat (l : LazyList (LazyList α)) : LazyList α :=
   match l with
-  | .nil => .nil
-  | .cons x l' => append x (concat l'.get)
+  | lnil => lnil
+  | .lcons x l' => append x (concat l'.get)
 
 /-- Bind for `LazyList`s is just `concatMap` (same as the list monad) -/
 def bindLazyList (l : LazyList α) (f : α → LazyList β) : LazyList β :=
@@ -86,13 +82,13 @@ instance : Applicative LazyList where
 
 /-- `Alternative` instance for `LazyList`s, where `xs <|> ys` is just `LazyList` append -/
 instance : Alternative LazyList where
-  failure := .nil
+  failure := lnil
   orElse xs f := append xs (f ())
 
 /-- Creates a lazy list by repeatedly applying a function `s` to generate a sequence of elements -/
 def lazySeq (s : α → α) (lo : α) (len : Nat) : LazyList α :=
   match len with
-  | .zero => .nil
-  | .succ len' => .cons lo (lazySeq s (s lo) len')
+  | .zero => lnil
+  | .succ len' => .lcons lo (lazySeq s (s lo) len')
 
 end LazyList
