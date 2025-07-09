bd329584a73977bf1d978667a99fad9dbd299ace
ngernest <eyn5@cornell.edu>
2025-07-09 18:58:46 -0400

    Helper for rewriting exprs into constructor application form

--- Plausible/GeneratingGoodGenerators/DeriveSubGenerator.lean
+++ Plausible/GeneratingGoodGenerators/DeriveSubGenerator.lean
@@ -7,15 +7,15 @@ open Lean
 -- Implements figure 4 from "Generating Good Generators for Inductive Relations", POPL '18
 --------------------------------------------------------------------------------------------
 
-/-- Creates the initial constraint map κ where all inputs are `Fixed` and the output is `Undef` -/
+/-- Creates the initial constraint map κ where all inputs are `Fixed`, the output & all universally-quantified variables is `Undef`
+    - `forAllVariables` is a list of (variable name, variable type) pairs -/
 def mkInitialConstraintMap (inputNames: List Name) (outputName : Name) (outputType : Expr) (forAllVariables : List (Name × Expr)): ConstraintMap :=
-  -- All inputs are fixed
   let inputConstraints := inputNames.zip (List.replicate inputNames.length .Fixed)
   let outputConstraints := [(outputName, .Undef outputType)]
   let forAllVarsConstraints := (fun (x, ty) => (x, .Undef ty)) <$> forAllVariables
   Std.HashMap.ofList $ inputConstraints ++ outputConstraints ++ forAllVarsConstraints
 
-/-- Creates the initial `UnifyState` -/
+/-- Creates the initial `UnifyState` where the constraint map is created using `mkInitialConstraintMap` -/
 def mkInitialUnifyState (inputNames : List Name) (outputName : Name) (outputType : Expr) (forAllVariables : List (Name × Expr)) : UnifyState :=
   let forAllVarNames := Prod.fst <$> forAllVariables
   let constraints := mkInitialConstraintMap inputNames outputName outputType forAllVariables
@@ -26,3 +26,11 @@ def mkInitialUnifyState (inputNames : List Name) (outputName : Name) (outputType
     patterns := []
     unknowns := unknowns
   }
+
+/-- Converts a expression `e` to a *constructor expression* `C r1 … rn`,
+    where `C` is a constructor and the `ri` are arguments,
+    returning `some (C, #[r1, …, rn])`
+    - If `e` is not an application, this function returns `none`  -/
+def convertToCtorExpr (e : Expr) : Option (Name × Array Expr) :=
+  if !e.isApp then none
+  else some e.getAppFnArgs
