0d8f286c8342e60ab7c108b9e17a91e3da4ef79c
ngernest <eyn5@cornell.edu>
2025-07-09 18:31:36 -0400

    Create initial UnifyState

--- Plausible/GeneratingGoodGenerators/DeriveSubGenerator.lean
+++ Plausible/GeneratingGoodGenerators/DeriveSubGenerator.lean
@@ -7,10 +7,22 @@ open Lean
 -- Implements figure 4 from "Generating Good Generators for Inductive Relations", POPL '18
 --------------------------------------------------------------------------------------------
 
-/-- Creates the initial constraint map κ where all inputs are `Fixed` and the output is `Undef`
-    - Note: we don't handle universally quantified variables `x` for now -/
-def mkInitialConstraintMap (inputNames: List Name) (outputName : Name) (outputType : Expr) : ConstraintMap :=
+/-- Creates the initial constraint map κ where all inputs are `Fixed` and the output is `Undef` -/
+def mkInitialConstraintMap (inputNames: List Name) (outputName : Name) (outputType : Expr) (forAllVariables : List (Name × Expr)): ConstraintMap :=
   -- All inputs are fixed
   let inputConstraints := inputNames.zip (List.replicate inputNames.length .Fixed)
   let outputConstraints := [(outputName, .Undef outputType)]
-  Std.HashMap.ofList $ inputConstraints ++ outputConstraints
+  let forAllVarsConstraints := (fun (x, ty) => (x, .Undef ty)) <$> forAllVariables
+  Std.HashMap.ofList $ inputConstraints ++ outputConstraints ++ forAllVarsConstraints
+
+/-- Creates the initial `UnifyState` -/
+def mkInitialUnifyState (inputNames : List Name) (outputName : Name) (outputType : Expr) (forAllVariables : List (Name × Expr)) : UnifyState :=
+  let forAllVarNames := Prod.fst <$> forAllVariables
+  let constraints := mkInitialConstraintMap inputNames outputName outputType forAllVariables
+  let unknowns := Std.HashSet.ofList (outputName :: (inputNames ++ forAllVarNames))
+  {
+    constraints := constraints
+    equalities := ∅
+    patterns := []
+    unknowns := unknowns
+  }
