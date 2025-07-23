43d67495c496db19c9d29946f428c8adee3d2398
ngernest <eyn5@cornell.edu>
2025-07-23 11:01:08 -0400

    Utils for determining whether outputs & inputs are under the same constructor (per Segev)

--- Plausible/GeneratingGoodGenerators/ScheduleUtils.lean
+++ Plausible/GeneratingGoodGenerators/ScheduleUtils.lean
@@ -1,25 +1,67 @@
 import Lean
 import Plausible.IR.Examples
-
-open Plausible.IR
+import Plausible.GeneratingGoodGenerators.UnificationMonad
+import Plausible.GeneratingGoodGenerators.DeriveSubGenerator
 
 open Lean Meta
+open Plausible.IR
 
-/-- Extracts all the unique variables that appear in a hypothesis of an inductive relation constructor
+/-- Extracts all the unique variable names that appear in a hypothesis of a constructor for an inductive relation
     (this looks underneath constructor applications).
 
     For example, given `typing Γ (type.Abs τ1 e) (type.Fun τ1 τ2)`,
     this function returns `[Γ, τ1, e, τ2]`.
  -/
-partial def extractVariablesInHypothesis (term : TSyntax `term) : MetaM (List Name) :=
+partial def variablesInHypothesisTSyntax (term : TSyntax `term) : MetaM (List Name) :=
   match term with
   | `($id:ident) => return [id.getId.eraseMacroScopes]
   | `($_:ident $args:term*)
   | `(($_:ident $args*)) => do
-    let foo ← args.toList.flatMapM extractVariablesInHypothesis
+    -- Note that we have to explicitly pattern match on parenthesized constructor applications,
+    -- otherwise we won't be able to handle nested constructor applications, e.g. `typing Γ (type.Abs τ1 e) (type.Fun τ1 τ2)`
+    let foo ← args.toList.flatMapM variablesInHypothesisTSyntax
     return (List.eraseDups foo)
   | _ => return []
 
+/-- Extracts all the unique variable names that appear in a `ConstructorExpr`
+    (this looks underneath constructor applications). -/
+def variablesInConstructorExpr (ctorExpr : ConstructorExpr) : List Name :=
+  match ctorExpr with
+  | .Unknown u => [u]
+  | .Ctor _ args => List.eraseDups $ args.flatMap variablesInConstructorExpr
 
-#eval show MetaM _ from do
-  extractVariablesInHypothesis (← `(typing Γ (type.Abs τ1 e) (type.Fun τ1 τ2)))
+-- #eval show MetaM _ from do
+--   extractVariablesInHypothesis (← `(typing Γ (type.Abs τ1 e) (type.Fun τ1 τ2)))
+
+/-- Determines whether inputs & outputs of a generator appear under the same constructor in a hypothesis `hyp`
+    - Example: consider the `TApp` constructor for STLC (when we are generating `e` such that `typing Γ e τ` holds):
+    ```
+    | TApp: ∀ Γ e1 e2 τ1 τ2,
+      typing Γ e2 τ1 →
+      typing Γ e1 (.Fun τ1 τ2) →
+      typing Γ (.App e1 e2) τ2
+    ```
+    The hypothesis `typing Γ e1 (.Fun τ1 τ2)` contains a term `.Fun τ1 τ2` where
+    the existentially quantified variable `τ1` hasn't been generated yet,
+    whereas `τ2` is an input to the generator (since it appears in the conclusion of `TApp`).
+    Since `τ1, τ2` both appear under the same `.Fun` constructor,
+    `outputInputNotUnderSameConstructor (.Fun τ1 τ2) [τ2]` returns `false`.  -/
+def outputInputNotUnderSameConstructor (hyp : Name × List ConstructorExpr) (outputVars : List Name) : Bool :=
+  let (_, args) := hyp
+  not $ List.any args (fun arg =>
+    let vars := variablesInConstructorExpr arg
+    List.any vars (. ∈ outputVars) && List.any vars (. ∉ outputVars))
+
+-- #eval outputInputNotUnderSameConstructor (`typing, [.Unknown `Γ, .Unknown `e1, .Ctor `Fun [.Unknown `τ1, .Unknown `τ2]]) [`τ2]
+
+/-- Determines whether the variables in `outputVars` are constrained by a function application in the hypothesis `hyp`.
+    This function is necessary since we can't output something and then assert that it equals the output of a (non-constructor) function
+    (since we don't have access to the function). -/
+partial def outputsNotConstrainedByFunctionApplication (hyp : Name × List ConstructorExpr) (outputVars : List Name) : Bool :=
+  let (_, args) := hyp
+  not $ List.any args (fun arg => aux false arg)
+    where
+      aux (b : Bool) (arg : ConstructorExpr) :=
+        match arg with
+        | .Unknown u => u ∈ outputVars
+        | .Ctor _ args => List.any args (aux b)
