430533cae1d4fab40581ef5654aa65e242a6d042
ngernest <eyn5@cornell.edu>
2025-07-30 11:56:22 -0400

    Add counterexample property for BitVec generator test

--- Test/DeriveArbitrary/BitVecStructureTest.lean
+++ Test/DeriveArbitrary/BitVecStructureTest.lean
@@ -1,6 +1,7 @@
 import Plausible.Arbitrary
 import Plausible.DeriveArbitrary
 import Plausible.Attr
+import Plausible.Testable
 
 open Plausible Gen
 
@@ -10,6 +11,7 @@ set_option guard_msgs.diff true
     whose index does not appear in the overall type (`DummyInductive`) -/
 inductive DummyInductive where
   | FromBitVec : ∀ (n : Nat), BitVec n → String → DummyInductive
+  deriving Repr
 
 set_option trace.plausible.deriving.arbitrary true in
 /--
@@ -57,3 +59,30 @@ deriving instance Arbitrary for DummyInductive
 /-- info: instArbitraryOfArbitraryFueled -/
 #guard_msgs in
 #synth Arbitrary DummyInductive
+
+/-- Shrinker for `DummyInductive` -/
+def shrinkDummyInductive : DummyInductive → List DummyInductive
+  | .FromBitVec n bitVec str =>
+    let shrunkenBitVecs := Shrinkable.shrink bitVec
+    let shrunkenStrs := Shrinkable.shrink str
+    (fun (bv, s) => .FromBitVec n bv s) <$> List.zip shrunkenBitVecs shrunkenStrs
+
+/-- `Shrinkable` instance for `DummyInductive` -/
+instance : Shrinkable DummyInductive where
+  shrink := shrinkDummyInductive
+
+/-- `SampleableExt` instance for `DummyInductive` -/
+instance : SampleableExt DummyInductive :=
+  SampleableExt.mkSelfContained Arbitrary.arbitrary
+
+/-- To test whether the derived generator can generate counterexamples,
+    we state an (erroneous) property that states that all `BitVec` arguments
+    to `DummyInductive.FromBitVec` represent the `Nat` 2, and see
+    if the derived generator can refute this property. -/
+def BitVecEqualsTwo : DummyInductive → Bool
+  | .FromBitVec _ bitVec _ => bitVec.toNat == 2
+
+/-- error: Found a counter-example! -/
+#guard_msgs in
+#eval Testable.check (∀ ind : DummyInductive, BitVecEqualsTwo ind)
+  (cfg := {numInst := 10, maxSize := 5, quiet := true})
