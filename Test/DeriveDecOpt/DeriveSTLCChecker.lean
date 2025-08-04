8d79d34700130422f1d302cd555bb3d67bf8fc26
ngernest <eyn5@cornell.edu>
2025-08-04 16:17:08 -0400

    fix failing test

--- Test/DeriveDecOpt/DeriveSTLCChecker.lean
+++ Test/DeriveDecOpt/DeriveSTLCChecker.lean
@@ -39,10 +39,36 @@ info: Try this checker: instance : DecOpt (typing Γ e τ) where
   decOpt :=
     let rec aux_dec (initSize : Nat) (size : Nat) (Γ_1 : List type) (e_1 : term) (τ_1 : type) : Option Bool :=
       match size with
-      | Nat.zero => DecOpt.checkerBacktrack []
+      | Nat.zero =>
+        DecOpt.checkerBacktrack
+          [fun _ =>
+            match e_1, τ_1 with
+            | term.Const n, type.Nat => Option.some Bool.true
+            | _, _ => Option.some Bool.false,
+            fun _ =>
+            match e_1 with
+            | term.Var x => DecOpt.decOpt (lookup Γ_1 x τ_1) initSize
+            | _ => Option.some Bool.false]
       | Nat.succ size' =>
         DecOpt.checkerBacktrack
           [fun _ =>
+            match e_1, τ_1 with
+            | term.Const n, type.Nat => Option.some Bool.true
+            | _, _ => Option.some Bool.false,
+            fun _ =>
+            match e_1, τ_1 with
+            | term.Add e1 e2, type.Nat =>
+              DecOpt.andOptList [aux_dec initSize size' Γ_1 e1 type.Nat, aux_dec initSize size' Γ_1 e2 type.Nat]
+            | _, _ => Option.some Bool.false,
+            fun _ =>
+            match e_1, τ_1 with
+            | term.Abs τ1 e, type.Fun τ1_0 τ2 => aux_dec initSize size' (τ1 :: Γ_1) e τ2
+            | _, _ => Option.some Bool.false,
+            fun _ =>
+            match e_1 with
+            | term.Var x => DecOpt.decOpt (lookup Γ_1 x τ_1) initSize
+            | _ => Option.some Bool.false,
+            fun _ =>
             match e_1 with
             | term.App e1 e2 =>
               EnumeratorCombinators.enumeratingOpt (EnumSuchThat.enumST (fun τ1 => typing Γ_1 e2 τ1))
