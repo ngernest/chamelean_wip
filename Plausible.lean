2d54af1ac6c9556af869160032dd1f6a204b0f73
ngernest <eyn5@cornell.edu>
2025-08-08 14:53:12 -0400

    Inline imports in Chamelean.lean in Plausible.lean

--- Plausible.lean
+++ Plausible.lean
@@ -11,7 +11,29 @@ import Plausible.Functions
 import Plausible.Attr
 import Plausible.Tactic
 
+-- Source files for Chamelean
+-- (extension to Plausible for deriving generators, enumerators & checkers)
+import Plausible.Chamelean.DeriveChecker
+import Plausible.Chamelean.MakeConstrainedProducerInstance
+import Plausible.Chamelean.OptionTGen
+import Plausible.Chamelean.Idents
+import Plausible.Chamelean.TSyntaxCombinators
+import Plausible.Chamelean.DecOpt
+import Plausible.Chamelean.GeneratorCombinators
+import Plausible.Chamelean.ArbitrarySizedSuchThat
+import Plausible.Chamelean.Arbitrary
+import Plausible.Chamelean.Enumerators
+import Plausible.Chamelean.EnumeratorCombinators
+import Plausible.Chamelean.LazyList
+import Plausible.Chamelean.Examples.STLC
+import Plausible.Chamelean.Examples.Trees
+import Plausible.Chamelean.DeriveArbitrary
+import Plausible.Chamelean.DeriveEnum
+import Plausible.Chamelean.Utils
+import Plausible.Chamelean.Debug
+import Plausible.Chamelean.UnificationMonad
+import Plausible.Chamelean.DeriveConstrainedProducer
+import Plausible.Chamelean.Schedules
+import Plausible.Chamelean.DeriveSchedules
+import Plausible.Chamelean.MExp
 import Plausible.Chamelean.Examples.ExampleInductiveRelations
-
-
-import Plausible.Chamelean.Chamelean
